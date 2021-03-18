32ad7616337d1d867b617b39d7a96fe7c7d1e897
unknown <steven.p.ruzicka@gmail.com>
2021-03-18 09:35:02 -0500

    structure of mvc

--- src/main/java/com/example/unicorn/DemoApplication.java
+++ src/main/java/com/example/unicorn/DemoApplication.java
@@ -8,13 +8,6 @@ import org.springframework.stereotype.*;
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }

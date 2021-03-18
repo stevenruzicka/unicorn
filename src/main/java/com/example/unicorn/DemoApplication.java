2fdf1eba061096a0d894d073defaae9bf206075a
unknown <steven.p.ruzicka@gmail.com>
2021-03-18 11:31:17 -0500

    structure of mvc

--- src/main/java/com/example/unicorn/DemoApplication.java
+++ src/main/java/com/example/unicorn/DemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.context.annotation.ComponentScan;
 
 @Controller
 @SpringBootApplication
+@ComponentScan(basePackages={"com.example"})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);

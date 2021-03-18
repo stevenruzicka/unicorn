bc57253274da18b3c61f257fcfa01edd8953c6f9
unknown <steven.p.ruzicka@gmail.com>
2021-03-18 15:00:55 -0500

    test

--- src/main/java/com/example/unicorn/Location.java
+++ src/main/java/com/example/unicorn/Location.java
@@ -18,6 +18,6 @@ public class Location {
     }
 
     public String toString() {
-        return this.name + " with " + (this.unicorns != null ? this.unicorns.size() + " unicorns" : "by themself");
+        return this.name + " (" + ((this.unicorns != null && this.unicorns.size() > 0) ? this.unicorns.size() + " unicorns)" : "(by themself)");
     }
 }

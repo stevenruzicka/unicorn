0defd831b5805035680d0a00e57beaf80055a8ef
unknown <steven.p.ruzicka@gmail.com>
2021-03-18 11:41:18 -0500

    structure of mvc

--- src/main/java/com/example/unicorn/LocationController.java
+++ src/main/java/com/example/unicorn/LocationController.java
@@ -27,7 +27,7 @@ public class LocationController {
         //m.addAttribute("unicorns", unicornRepo.findAll());
         m.addAttribute("locations", locations);
         //m.addAttribute("locations", locationRepo.findAll());
-        return "location";
+        return "locations";
     }
 
     @RequestMapping(value="/locations", method=RequestMethod.POST)
